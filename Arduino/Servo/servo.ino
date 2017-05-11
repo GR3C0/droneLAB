eed5fcb58ebc5b64057d754a2ed423fe4b1eaeab
GR3C0 <diegomorellmasip@gmail.com>
2017-05-11 23:31:43 +0200

    mas motores

--- Arduino/Servo/servo.ino
+++ Arduino/Servo/servo.ino
@@ -1,10 +1,7 @@
 #include  <Servo.h>
  
 Servo ESC; //Crear un objeto de clase servo
- 
-int vel = 1000; //amplitud del pulso
- 
- 
+   
 void setup()
 {
   //Asignar un pin al ESC
@@ -14,11 +11,9 @@ void setup()
   ESC.writeMicroseconds(2000); //1000 = 1ms
   //Cambia el 1000 anterior por 2000 si
   //tu ESC se activa con un pulso de 2ms
-  delay(5000); //Esperar 5 segundos para hacer la activacion
    
   //Iniciar puerto serial
   Serial.begin(9600);
-  Serial.setTimeout(10);
    
    
 }
@@ -26,13 +21,13 @@ void setup()
  
 void loop()
 {
-  if(Serial.available() >= 1)
-  {
-    vel = Serial.parseInt(); //Leer un entero por serial
-    if(vel != 0)
-    {
-      ESC.writeMicroseconds(vel); //Generar un pulso con el numero recibido
-    }
-  }
+      ESC.writeMicroseconds(1200);
+      delay(2000);
+      ESC.writeMicroseconds(1400);
+      delay(2000);
+      ESC.writeMicroseconds(1500);
+      delay(2000);
+      ESC.writeMicroseconds(1990);
+
 }
 
--- Arduino/Servo/servo2.ino
+++ Arduino/Servo/servo2.ino
@@ -1,27 +1,36 @@
+// #include <Servo.h>
 
-#include <Servo.h>
+// Servo esc;
 
-Servo myservo;         //Declaramos un objeto de tipo servo
-int velocidad = 0;     //Declaramos y inicializamos la variable que controlará la velocidad
+// int ACELERAMAX;
+// int ACELERAMED;
+// int ACELERAMIN;
+// void setup(){
+//   pinMode (2,INPUT);
+//   pinMode (3,INPUT);
+//   pinMode (4,INPUT);
+//   esc.attach (9);
+//   // esc.writeMicroseconds(0);
+//   Serial.begin (9600);
+// }
+// void loop() {
+//   ACELERAMIN = digitalRead(2);
+//   ACELERAMED = digitalRead(3);
+//   ACELERAMAX = digitalRead(4);
 
-void setup()
-{
- Serial.begin(9600);                                              //Utilizo el monitor serie para comprovar los valores que está recibiendo el motor (ésta velocidad es por el tema del mando).
- myservo.writeMicroseconds(2000);
- myservo.attach(9);
- arm();                                                             // Importantísima funcion: Es la funcion con la que conseguimos el armado (Y pitido) del ESC (Está al final del programa).
-}
-void loop()
-{
- {
-     velocidad = map(0, 118 , 0, 40, 90);         //Con ésto yo controlo el motor: hago un mapeado que consta de 118-(valor con el que empiezo) 0-(Valor de gas al maximo) 40- (VALOR MINIMO QUE NECESITA EL MOTOR PARA MOVERSE) 90-(Valor maximo que le dejo girar al motor.)
-     myservo.write(velocidad);                                              //En el mapeado anterior meto cada valor a la variable velocidad constantemente y ahora se la paso al ESC.
-     delay(15);                                                             // delay necesario
- }
-}
-void arm(){                                                                   // Funcion del armado del ESC
- myservo.write(0);
- delay(100);
- myservo.write(20);
- delay(100);
-}
+//   if(ACELERAMAX == 0) {
+//     esc.writeMicroseconds(1990);
+//     Serial.println ("Max");
+//   }
+
+//   else if(ACELERAMED == 0) {
+//    esc.writeMicroseconds(1470);
+//     Serial.println ("Med");
+//   }
+
+//   else if(ACELERAMIN == 0) {
+//    esc.writeMicroseconds(1130);
+//    Serial.println ("Min");
+//   }
+
+// }
