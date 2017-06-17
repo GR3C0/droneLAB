8b52969a55470f3d60067dca25d09885c028d721
GR3C0 <diegomorellmasip@gmail.com>
2017-06-17 03:38:32 +0200

    Hecha la función que medirá la fuerza sometida al drone

--- Arduino/Arduino/General/src/general.ino
+++ Arduino/Arduino/General/src/general.ino
@@ -9,6 +9,7 @@
 #include <Servo.h>
 
 Servo motor;
+float masa = 2; // Masa del drone
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55); // Llamamos al Sensor
 
@@ -21,9 +22,6 @@ void setup()
     while(1);
   }
   bno.setExtCrystalUse(true);
-
-
-
 }
 
 void loop()
@@ -33,22 +31,22 @@ void loop()
 }
 
 
-float sensor() // Función del acelerometro
+float rozamiento_cero() // Función de rozamiento cero
 {
-  Serial.println("Prubea de fuerza ejercida");
-  imu::Vector<3> acelerometro = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER); // Los datos del acelerometro
-  for (size_t t = 0; t < 5000; t++) {
-    Serial.println(t);
-  }
-  int x;
-  while (x < 5000)
-  {
-    x++;
-    if (x
-       == 5000)
-    {
-      Serial.println("Han pasado 5 segundos");
-    }
-  }
+  imu::Vector<3> acelerometro = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER); // Obtiene la aceleración
+  unsigned long t_inicio = millis(); // Obtiene el tiempo de ejecución del programa al iniciarse, este valor es 0
+  unsigned long t_final; // Variable que almacenará el tiempo final para restarlo al inicial
+  // CAMBIAR LOS VALORES SEGÚN LAS NECESIDADES
+  float ax_min = 9.5;
+  float ay_min = 9.5;
+  float az_min = 9.5;
 
+  Serial.println(acelerometro.x()); // Solo para pruebas
+  if (acelerometro.x()>ax_min || acelerometro.y()>ay_min || acelerometro.z()>az_min) // Si uno de los tres ejes sufre una aceleración
+  {
+    t_final = millis();
+  }
+  // Aquí va el tema de motores
+  unsigned long aceleracion = t_final-t_inicio;
+  float Fuerza_motores = aceleracion * masa;
 }
