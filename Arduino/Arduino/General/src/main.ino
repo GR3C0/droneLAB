eb97f9fe8cfaf3bf94577507426d921dfd237125
GR3C0 <diegomorellmasip@gmail.com>
2017-10-19 18:03:58 +0200

    Cambios grandes

--- Arduino/Arduino/General/src/main.ino
+++ Arduino/Arduino/General/src/main.ino
@@ -4,22 +4,19 @@
 #include "Configuracion.h"
 #include <NewPing.h>
 #include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 #include <Servo.h>
 
-Servo motor;
+Servo motor_der;
+Servo motor_izq;
 float masa = 2; // Masa del drone
 
-Adafruit_BNO055 bno = Adafruit_BNO055(55); // Llamamos al Sensor
-
-NewPing sonar[SONAR_NUM] = // Declaramos todos los sonars que hay
+NewPing sonar[SONAR_NUM] =
 {
-	NewPing(SONAR_1, TRIG_SONAR_1, MAX_DISTANCE), // Todos los pines están declarados en
-	NewPing(SONAR_2, TRIG_SONAR_2, MAX_DISTANCE), // Configuracion.h
+	NewPing(SONAR_1, TRIG_SONAR_1, MAX_DISTANCE),
+	NewPing(SONAR_2, TRIG_SONAR_2, MAX_DISTANCE),
 	NewPing(SONAR_3, TRIG_SONAR_3, MAX_DISTANCE),
-  NewPing(SONAR_4, TRIG_SONAR_4, MAX_DISTANCE),
+  NewPing(SONAR_4,TRIG_SONAR_4, MAX_DISTANCE),
 };
 
 
@@ -38,20 +35,7 @@ void loop()
 {
   // Dependiendo de lo que el arduino reciba que quiere hacer el usuario,
   // Usa una función o otra
-  while (!Serial.available()); // Comprueba si el puerto para leer datos está abierto
-  int respuesta;
-  respuesta = Serial.read(); // Lectura de la respuesta del usuario
-  Serial.println("Rozamiento cero 1");
-  Serial.println("Sonars 2");
 
-  if (respuesta == 1)
-  {
-    rozamiento_cero();
-  }
-  else if (respuesta == 2)
-  {
-      sonars();
-  }
 }
 
 void rozamiento_cero()
@@ -61,8 +45,8 @@ void rozamiento_cero()
   unsigned long t_final; // Variable que almacenará el tiempo final para restarlo al inicial
   Serial.println(acelerometro.x()); // Solo para pruebas
 
-  if (acelerometro.x()>AX_MIN || acelerometro.y()>AY_MIN || acelerometro.z()>AZ_MIN) // Si uno de los tres ejes sufre una aceleración
-  {
+  if (acelerometro.x()>AX_MIN || acelerometro.y()>AY_MIN || acelerometro.z()>AZ_MIN) // Si uno de los tres ejes
+	{																																										// sufre una aceleración
     t_final = millis();
   }
   // Aquí va el tema de motores
@@ -73,17 +57,120 @@ void rozamiento_cero()
 
 void sonars()
 {
-  // for(int i = 0; i < SONAR_NUM; i++)
-  //{ // Ciclo para recorrer la lista de sonars
-  //   int uS = sonar[i].ping_median(); // Lee el ping del sonar
-  //   int distancia = uS / US_ROUNDTRIP_CM; // Lo transforma a cm
-  //     Serial.print(distancia);
-  //     Serial.print("cm");
-  //     Serial.print("||");
-  //}
-  // int sonar1 = sonar[2].ping_median(); Así se coge solo los datos de un sonar
-  // int distancia1 = sonar1 / US_ROUNDTRIP_CM;
-  // Serial.println(distancia1);
+  for(int i = 0; i < SONAR_NUM; i++){
+    int uS = sonar[i].ping_median();
+    int distancia = uS / US_ROUNDTRIP_CM;
+      Serial.print(distancia);
+      Serial.print("cm");
+      Serial.print("||");
+  }
+  Serial.println();
 
+}
+
+void pid()
+{
+	Serial.print("Activado PID");
+		/////////////////////////////I M U/////////////////////////////////////
+	timePrev = time;  // Almacenamos otra vez el tiempo
+	time = millis();  // Lectura del tiempo actual
+	T_transcurrido = (time - timePrev) / 1000; // Se calcula el tiempo pasado en segundos
+
+	// Lectura de la aceleracion y los angulos
+	imu::Vector<3> acelerometro = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+	imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+	imu::Vector<3> giroscopio = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+
+
+	// Obtenemos los grados
+
+	Serial.print(euler.x());
+	Serial.print("||");
+	Serial.print(euler.y());
+	Serial.print("||");
+	Serial.println(euler.z());
+
+	// Datos del giroscopio pasados a grados/s
+
+	// giro_z = giroscopio.z()*57.2958;
+	// giro_y = giroscopio.y()*57.2958;
+	giro_z = euler.z();
+	giro_y = euler.y();
+
+	acel_z = acelerometro.z();
+	acel_y = acelerometro.y();
+
+	Angulo_aceleracion[0] = acel_z;
+	Angulo_aceleracion[1] = acel_y;
+	Angulo_giro[0] = giro_z;
+	Angulo_giro[1] = giro_y;
+
+	/*---Ángulo Z---*/
+	// Angulo_total[0] = 0.98 *(Angulo_total[0] + Angulo_giro[0]*T_transcurrido) + 0.02*Angulo_aceleracion[0];
+	/*---Ángulo Y---*/
+	// Angulo_total[1] = 0.98 *(Angulo_total[1] + Angulo_giro[1]*T_transcurrido) + 0.02*Angulo_aceleracion[1];
+
+	// Almacenamos el error
+	// error = Angulo_total[0]-angulo_deseado;
+	error = Angulo_giro[0] - angulo_deseado;
+
+	// Esta es la constante que solo hace falta multiplicarla por el error
+	pid_p = kp*error;
+
+	// La integral solo debe actuar si el error es mínimo
+	if(-3 < error || error < 3)
+	{
+		pid_i = pid_i+(ki*error);
+	}
+
+	// La derivada
+	pid_d = kd*((error - error_previo)/T_transcurrido);
+
+	// Almacenamos todo el PID
+	PID = pid_p + pid_i + pid_d;
+
+	// Como la velocidad de los motores solo puede ir entre 1000 y 2000 hacemos unos ajustes para que no se vuelva loco
+	if(PID < -1000)
+	{
+		PID=-1000;
+	}
+	if(PID > 1000)
+	{
+		PID=1000;
+	}
+
+	// Finalmente le decimos a los motores que hacer
+	pwmIzq = velocidad + PID;
+	pwmDer = velocidad - PID;
+
+	// Reajustamos la velocidad por si acaso
+	if(pwmDer < 1200)
+	{
+		pwmDer= 1200;
+	}
+	if(pwmDer > 2000)
+	{
+		pwmDer=2000;
+	}
+
+	if(pwmIzq < 1200)
+	{
+		pwmIzq= 1200;
+	}
+	if(pwmIzq > 2000)
+	{
+		pwmIzq=2000;
+	}
+
+	// Le enviamos los datos a los motores
+	motor_der.writeMicroseconds(pwmIzq);
+	Serial.print(pwmIzq);
+	Serial.print("||");
+	motor_izq.writeMicroseconds(pwmDer);
+	Serial.println(pwmDer);
+	error_previo = error; // Almacenamos el error
+
+	motor_der.writeMicroseconds(1700);
+	motor_izq.writeMicroseconds(1700);
 
 }
